60a5fa6250065bbebd20e48f38806d567d71ece3
nieguoxin <nieguoxin@huobi.com>
2021-06-23 18:56:45 +0800

    add exclude

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-'use strict';
+'use strict'
 
-var postcss = require('postcss');
-var objectAssign = require('object-assign');
+var postcss = require('postcss')
+var objectAssign = require('object-assign')
 
 var defaults = {
   unitToConvert: 'px',
@@ -9,16 +9,24 @@ var defaults = {
   viewportHeight: 568, // not now used; TODO: need for different units and math for different properties
   unitPrecision: 5,
   viewportUnit: 'vw',
-  fontViewportUnit: 'vw',  // vmin is more suitable.
+  fontViewportUnit: 'vw', // vmin is more suitable.
   selectorBlackList: [],
   minPixelValue: 1,
-  mediaQuery: false
-};
+  mediaQuery: false,
+}
 
 module.exports = postcss.plugin('postcss-px-to-viewport', function (options) {
+  var opts = objectAssign({}, defaults, options)
 
-  var opts = objectAssign({}, defaults, options);
-  var pxReplace = createPxReplace(opts.viewportWidth, opts.minPixelValue, opts.unitPrecision, opts.viewportUnit, opts.ignoreUnit);
+  checkRegExpOrArray(opts, 'exclude')
+
+  var pxReplace = createPxReplace(
+    opts.viewportWidth,
+    opts.minPixelValue,
+    opts.unitPrecision,
+    opts.viewportUnit,
+    opts.ignoreUnit
+  )
 
   // excluding regex trick: http://www.rexegg.com/regex-best-trick.html
   // Not anything inside double quotes
@@ -26,65 +34,129 @@ module.exports = postcss.plugin('postcss-px-to-viewport', function (options) {
   // Not anything inside url()
   // Any digit followed by px
   // !singlequotes|!doublequotes|!url()|pixelunit
-  var pxRegex = new RegExp('"[^"]+"|\'[^\']+\'|url\\([^\\)]+\\)|(\\d*\\.?\\d+)' + '(?:' + opts.unitToConvert + '|' + opts.ignoreUnit + ')', 'ig')
+  var pxRegex = new RegExp(
+    '"[^"]+"|\'[^\']+\'|url\\([^\\)]+\\)|(\\d*\\.?\\d+)' +
+      '(?:' +
+      opts.unitToConvert +
+      '|' +
+      opts.ignoreUnit +
+      ')',
+    'ig'
+  )
 
   return function (css) {
+    css.walkRules(function (rule) {
+      // Add exclude option to ignore some files like 'node_modules'
+      var file = rule.source && rule.source.input.file
 
-    css.walkDecls(function (decl, i) {
-      // This should be the fastest test and will remove most declarations
-      if (decl.value.indexOf(opts.unitToConvert) === -1 && decl.value.indexOf(opts.ignoreUnit) === -1) return;
-
-      var hasBlacklistedSelector = !!blacklistedSelector(opts.selectorBlackList, decl.parent.selector);
-      var unit = getUnit(decl.prop, opts);
-      var next = decl.next();
-      var hasIgnoreComment = false;
-      if (next && next.type === 'comment' && opts.ignoreComment) {
-        if (next.text === opts.ignoreComment) {
-          hasIgnoreComment = true;
+      if (opts.exclude && file) {
+        if (Object.prototype.toString.call(opts.exclude) === '[object RegExp]') {
+          if (opts.exclude.test(file)) return
+        } else if (Object.prototype.toString.call(opts.exclude) === '[object Array]') {
+          for (var i = 0; i < opts.exclude.length; i++) {
+            if (opts.exclude[i].test(file)) return
+          }
         }
       }
-      decl.value = decl.value.replace(pxRegex, createPxReplace(opts.viewportWidth, opts.minPixelValue, opts.unitPrecision, unit, opts.ignoreUnit, hasIgnoreComment, hasBlacklistedSelector));
-    });
 
-    if (opts.mediaQuery) {
-      css.walkAtRules('media', function (rule) {
-        if (rule.params.indexOf(opts.unitToConvert) === -1) return;
-        rule.params = rule.params.replace(pxRegex, pxReplace);
-      });
-    }
+      css.walkDecls(function (decl, i) {
+        // This should be the fastest test and will remove most declarations
+        if (
+          decl.value.indexOf(opts.unitToConvert) === -1 &&
+          decl.value.indexOf(opts.ignoreUnit) === -1
+        )
+          return
 
-  };
-});
+        var hasBlacklistedSelector = !!blacklistedSelector(
+          opts.selectorBlackList,
+          decl.parent.selector
+        )
+        var unit = getUnit(decl.prop, opts)
+        var next = decl.next()
+        var hasIgnoreComment = false
+        if (next && next.type === 'comment' && opts.ignoreComment) {
+          if (next.text === opts.ignoreComment) {
+            hasIgnoreComment = true
+          }
+        }
+        decl.value = decl.value.replace(
+          pxRegex,
+          createPxReplace(
+            opts.viewportWidth,
+            opts.minPixelValue,
+            opts.unitPrecision,
+            unit,
+            opts.ignoreUnit,
+            hasIgnoreComment,
+            hasBlacklistedSelector
+          )
+        )
+      })
+
+      if (opts.mediaQuery) {
+        css.walkAtRules('media', function (rule) {
+          if (rule.params.indexOf(opts.unitToConvert) === -1) return
+          rule.params = rule.params.replace(pxRegex, pxReplace)
+        })
+      }
+    })
+  }
+})
 
 function getUnit(prop, opts) {
-  return prop.indexOf('font') === -1 ? opts.viewportUnit : opts.fontViewportUnit;
+  return prop.indexOf('font') === -1 ? opts.viewportUnit : opts.fontViewportUnit
 }
 
-function createPxReplace(viewportSize, minPixelValue, unitPrecision, viewportUnit, ignoreUnit, hasIgnoreComment, hasBlacklistedSelector) {
+function createPxReplace(
+  viewportSize,
+  minPixelValue,
+  unitPrecision,
+  viewportUnit,
+  ignoreUnit,
+  hasIgnoreComment,
+  hasBlacklistedSelector
+) {
   return function (m, $1) {
-    if (!$1) return m;
+    if (!$1) return m
     if (m.indexOf(ignoreUnit) !== -1) {
-      return $1 + 'px';
+      return $1 + 'px'
     }
     if (hasIgnoreComment || hasBlacklistedSelector) {
-      return m;
+      return m
     }
-    var pixels = parseFloat($1);
-    if (pixels <= minPixelValue) return m;
-    return toFixed((pixels / viewportSize * 100), unitPrecision) + viewportUnit;
-  };
+    var pixels = parseFloat($1)
+    if (pixels <= minPixelValue) return m
+    return toFixed((pixels / viewportSize) * 100, unitPrecision) + viewportUnit
+  }
 }
 
 function toFixed(number, precision) {
   var multiplier = Math.pow(10, precision + 1),
-    wholeNumber = Math.floor(number * multiplier);
-  return Math.round(wholeNumber / 10) * 10 / multiplier;
+    wholeNumber = Math.floor(number * multiplier)
+  return (Math.round(wholeNumber / 10) * 10) / multiplier
 }
 
 function blacklistedSelector(blacklist, selector) {
-  if (typeof selector !== 'string') return;
+  if (typeof selector !== 'string') return
   return blacklist.some(function (regex) {
-    if (typeof regex === 'string') return selector.indexOf(regex) !== -1;
-    return selector.match(regex);
-  });
+    if (typeof regex === 'string') return selector.indexOf(regex) !== -1
+    return selector.match(regex)
+  })
+}
+
+function checkRegExpOrArray(options, optionName) {
+  var option = options[optionName]
+  if (!option) return
+  if (Object.prototype.toString.call(option) === '[object RegExp]') return
+  if (Object.prototype.toString.call(option) === '[object Array]') {
+    var bad = false
+    for (var i = 0; i < option.length; i++) {
+      if (Object.prototype.toString.call(option[i]) !== '[object RegExp]') {
+        bad = true
+        break
+      }
+    }
+    if (!bad) return
+  }
+  throw new Error('options.' + optionName + ' should be RegExp or Array of RegExp.')
 }
